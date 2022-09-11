f1674ccd4012c021970696ebe9f3c9a655662b26
Surya Saini <Surya0796@gmail.com>
2022-09-11 21:27:54 +0530

    animated circle

--- animation/canvas/canvas.js
+++ animation/canvas/canvas.js
@@ -12,42 +12,30 @@ const c = canvas.getContext("2d")
 // c.fillRect(300,400,100,100)
 
 
-// line
-// c.beginPath();
-// c.strokeStyle = "red"
-// c.moveTo(400,0)
-// c.lineTo(100,400)
-// c.lineTo(400,400)
-// c.lineTo(400,0)
-// c.stroke()
+
+var xvel = 6
+var yvel = 6;
+var y = Math.random() * window.innerHeight  
 
 
-for(let i = 0; i <100; i++){
-    // Random circles
-    let x = Math.random() * window.innerWidth
-    let y = Math.random() * window.innerHeight
-    // let lineX = Math.random() * window.innerWidth
-    // let lineY = Math.random() * window.innerHeight
-    let radius = Math.random() * 70
-    let red = Math.random() * 255
-    let green = Math.random() * 255
-    let blue = Math.random() * 255
 
-    radius = radius < 1 ? Math.random() * 100 : radius
-    c.beginPath();
-    c.strokeStyle = `rgb(${red},${green},${blue})`
-    c.arc(x,y,radius,0, Math.PI *Math.random() * 10,false)
+function animate(){
+    requestAnimationFrame(animate)
+    c.clearRect(0,0,innerWidth,innerHeight)
+    c.beginPath()
+    c.strokeStyle = "red"
+    c.arc(x,y,30,0,Math.PI * 2,false)
     c.stroke()
-
-
-    // Random lines
-    // c.beginPath();
-    // c.strokeStyle = "black"
-    // c.moveTo(x,y)
-    // c.lineTo(x*2,y*4)
-    // c.lineTo(x,y)
-    // c.lineTo(x+50,y+10)
-    // c.stroke()
+    if(x + 30 > window.innerWidth|| x - 30 < 0){
+        xvel = -xvel
+    }
+    if(y+30 > window.innerHeight || y-30 < 0){
+        yvel = -yvel
+    }
+    x += xvel
+    y += yvel
+    console.log("i")
 }
+animate()
 
 
