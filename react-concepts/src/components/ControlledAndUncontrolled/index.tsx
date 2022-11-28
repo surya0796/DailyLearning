1a47337d21bfd9ace9bd34ba060d866823087fe1
Surya Saini <Surya0796@gmail.com>
2022-11-28 15:06:23 +0530

    removed some unwanted files

---
+++ react-concepts/src/components/ControlledAndUncontrolled/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Form = () => {
+  return (
+    <div>
+        Make Controlled and Uncontrolled components
+
+    </div>
+  )
+}
+
+Form.propTypes = {}
+
+export default Form
